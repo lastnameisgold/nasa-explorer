7cc8da783fae8ea634ec1bf69b04d89c86f47a9a
Fernando <me@fdorado.com>
2023-03-01 17:33:09 -0500

    Added Unplash API to globals.js

--- nasa-explorer-app/src/globals.js
+++ nasa-explorer-app/src/globals.js
@@ -1,2 +1,3 @@
 export const APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=2XkAKyvL0yEM9tLQOx1ZzVSjFV93EO02r69p0f3Y"
 export const SEARCH_URL = "https://images-api.nasa.gov/search?q="
+export const UNSPLASH_URL = "https://api.unsplash.com/photos/random?&query=space&client_id=_ughVXlwJoG3c39KhLl2e_ANtQ26N9MzTTklLbBBQ14"
