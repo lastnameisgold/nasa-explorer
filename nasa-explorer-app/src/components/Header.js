2221f005391aa253b4ff4c12787b07cb692cd9d3
Fernando <me@fdorado.com>
2023-02-28 21:52:16 -0500

    Added SVG logo to navbar.

--- nasa-explorer-app/src/components/Header.js
+++ nasa-explorer-app/src/components/Header.js
@@ -1,12 +1,16 @@
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
+import logo from "../logo.svg"
 
 export default function Header() {
     return(
         <div className="nav-container">
-            <div className="logo-container">
-                <Link to="/"><h2>NASA Explorer</h2></Link>
-            </div>
+                <Link to="/">
+                    <div className="logo-container">
+                        <img className="nasa-logo" src={logo} alt="NASA logo" />
+                        <h2 className="explorer-logo">Explorer</h2>
+                    </div>
+                </Link>
                 <Nav />
         </div>
     )
