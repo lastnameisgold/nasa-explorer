1c36f1076402ded05d9b12e90cb33eb938560248
Fernando <me@fdorado.com>
2023-03-02 14:04:19 -0500

    Videos are working.

--- nasa-explorer-app/src/components/VideoDetails.js
+++ nasa-explorer-app/src/components/VideoDetails.js
@@ -13,12 +13,13 @@ export default function VideoDetails() {
         setVideo(selectedVideo)
     }, [videos, index])
 
+    const videoURL = video.data ? `http://images-assets.nasa.gov/video/${video.data[0].nasa_id}/${video.data[0].nasa_id}~orig.mp4` : null
 
-    return video ? (
+    return video && videoURL ? (
         <div className="details-container">
             <Link className="back-button" to="/videos"><span class="material-symbols-rounded icon-size">arrow_back</span>Return to Videos</Link>
             <div className="details-content-container">
-                <video src="http://images-assets.nasa.gov/video/{video.data[0].nasa_id}/{video.data[0].nasa_id}~orig.mp4" type="video/mp4" width="750" height="500" controls></video>
+                <video src={videoURL} type="video/mp4" width="750" height="500" controls></video>
                 <div className="details-text-content-container">
                     <h2>{video.data[0].title}</h2>
                     <p>{video.data[0].description}</p>
